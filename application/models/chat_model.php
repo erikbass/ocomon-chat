09b373d825eeb1f9a5c1c9b4b1f9fa9febbb1096
Erik <erikguimaraesdasilva@gmail.com>
2014-06-21 00:33:14 -0400

    Model chat class

---
+++ application/models/chat_model.php
@@ -0,0 +1,29 @@
+<?php
+
+	class Chat_model extends CI_Model{
+		var $id;
+		var $from;
+		var $to;
+		var $mensagem;
+		var $data;
+
+		function salvar($from, $to, $mensagem){
+			$data = array(
+			   'from' 		=> $from,
+			   'to' 		=> $to,
+			   'mensagem' 	=> $mensagem,
+			   'data' 		=> date("Y-m-d H:i:s")
+			);
+
+			$this->db->insert('chat', $data);
+			return;
+		}
+
+		function recuperaTodas(){
+			$this->db->select('mensagem, data');
+			$this->db->order_by('data','asc');
+			$result = $this->db->get('chat');
+
+			return $result->result();
+		}
+	}
